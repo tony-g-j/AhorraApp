3f6deae4b839224c702ebff5753cfcda74e5fee9
juliantomlinson91 <124048762@upq.edu.mx>
2025-11-02 20:56:58 -0600

    Creación de la screen para Inicio de Sesión

--- AhorraApp/screens/MenuScreen.js
+++ AhorraApp/screens/MenuScreen.js
@@ -2,21 +2,24 @@ import { Text, StyleSheet, Button, View } from "react-native";
 import React, { useState } from "react";
 import GraficaScreen from "./graficasScreen";
 import RegistroGastosScreen from "./registroGastosScreen";
+import InicioSesionScreen from "./InicioSesionScreen";
 
 export default function MenuScreen() {
   const [screen, setScreen] = useState("menu");
 
   switch (screen) {
-    case "inicioSesiónScreen":
-      //screen
-      break;
+    case "InicioSesionScreen":
+      return <InicioSesionScreen/>
+      
     case "registroScreen":
       //screen
       break;
     case "registroGastosScreen":
-      return <RegistroGastosScreen/>
-    case "graficasScreen":
-      return <GraficaScreen/>
+      //screen
+      break;
+    case "IngresosScreen":
+      //screen
+      break;
     case "EgresosScreen":
       //screen
       break;
@@ -29,7 +32,7 @@ export default function MenuScreen() {
           <Text style={styles.title}>Screens AhorraApp</Text>
           <View style={styles.btns}>
             <Button
-              onPress={() => setScreen("inicioSesiónScreen")}
+              onPress={() => setScreen("InicioSesionScreen")}
               title="Inicio de sesión"
             />
             <Button
@@ -41,8 +44,8 @@ export default function MenuScreen() {
               title="Registro de gastos"
             />
             <Button
-              onPress={() => setScreen("graficasScreen")}
-              title="Graficas"
+              onPress={() => setScreen("IngresosScreen")}
+              title="Ingresos"
             />
             <Button
               onPress={() => setScreen("EgresosScreen")}
