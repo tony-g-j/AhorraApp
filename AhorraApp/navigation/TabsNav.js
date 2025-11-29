0ce7c553e0d4059beaca64208426acfe192a54e3
FERNANDA JUAREZ <lizgar239@gmail.com>
2025-11-28 23:28:07 -0600

    Iconos

--- AhorraApp/navigation/TabsNav.js
+++ AhorraApp/navigation/TabsNav.js
@@ -2,15 +2,46 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import PresupuestosS from '../screens/PresupuestoScreen';
 import RegistroGastosS from '../screens/registroGastosScreen';
 import GraficasS from '../screens/graficasScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabsNav(){
     return(
-        <Tab.Navigator screenOptions={{headerShown: false}}>
-            <Tab.Screen name='Presupuestos' component={PresupuestosS} />
-            <Tab.Screen name='Graficas' component={GraficasS}/>
-            <Tab.Screen name='Registro' component={RegistroGastosS}/>
+        <Tab.Navigator 
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: '#2196F3',
+                tabBarInactiveTintColor: 'gray',
+            }}
+        >
+            <Tab.Screen 
+                name='Presupuestos' 
+                component={PresupuestosS}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="wallet-outline" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Tab.Screen 
+                name='Graficas' 
+                component={GraficasS}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="bar-chart-outline" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Tab.Screen 
+                name='Registro' 
+                component={RegistroGastosS}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="add-circle-outline" size={size} color={color} />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     )
 }
